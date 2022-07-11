5d7bf679c9e45bef6aaf4c2c76914d2e70356236
perlamcarlson <perlamcarlson@gmail.com>
2022-07-11 15:41:42 -0700

    saving progress

---
+++ ghi/app/src/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Wheel from './components/wheel';
+
+import './styles.css';
+
+export class App extends React.Component {
+    constructor() {
+        super();
+        this.places = []
+    }
+ }

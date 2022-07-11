f01730dc56f6387a530c5645421b6c28e6a3d762
perlamcarlson <perlamcarlson@gmail.com>
2022-07-11 15:56:03 -0700

    added Spinner.js

--- ghi/app/src/Spinner.js
+++ ghi/app/src/Spinner.js
@@ -8,6 +8,28 @@ import './styles.css';
 export class App extends React.Component {
     constructor() {
         super();
-        this.places = []
+        this.places = [
+            'Cardio',
+            'Chest',
+            'Back',
+            'Shoulders',
+            'Upper arms',
+            'Upper legs'
+        ]
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>What exercise should you do today?</h1>
+                <Wheel items={this.places} />
+            </div>
+        );
     }
  }
+
+ const rootElement = document.getElementById('root');
+ ReactDOM.render(<App />, rootElement);
+
+
+ export default Spinner;

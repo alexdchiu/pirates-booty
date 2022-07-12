edb9628f70b55a86ee9b817d8b910a7e5104a86b
perlamcarlson <perlamcarlson@gmail.com>
2022-07-12 15:41:09 -0700

    updated imports in App.js

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -11,7 +11,7 @@ function Nav() {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                    <NavLink className="nav-link" to="/main_page"> </NavLink>
+                    <NavLink className="nav-link" to="/MainPage"> </NavLink>
                 </li>
                 <li className="nav-item">
                     <NavLink className="nav-link" to="/spinner"></NavLink>

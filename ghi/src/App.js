51bd93efe26da51d62e469295304d494780e7108
perlamcarlson <perlamcarlson@gmail.com>
2022-07-12 15:30:32 -0700

    added routes on app.js

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
       </Routes>
+        <Route path="/" element={<Spinner />} />
     </div>
     
     </BrowserRouter>

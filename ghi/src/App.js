8ce5a9b5b544f984885cde81e250774b3ebe9973
perlamcarlson <perlamcarlson@gmail.com>
2022-07-13 12:40:47 -0700

    working on React

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Nav from './Nav';
 import MainPage from './MainPage';
-import Spinner from './Spinner';
+import Spinner from './component/Wheel/Spinner';
 
 
 function App() {
--- ghi/src/index.js
+++ ghi/src/index.js
@@ -3,13 +3,39 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Wheel from './components/wheel';
+import './styles.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export class App extends React.Component {
+  constructor() {
+    super();
+    this.places = [
+      'Cardio',
+      'Chest',
+      'Back',
+      'Shoulders',
+      'Upper arms',
+      'Upper legs'
+  ];
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>What should you eat today?</h1>
+        <Wheel items={this.places} />
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
+
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

93c132cdde67566b0e5a5c45fcdfa450025ca639
perlamcarlson <perlamcarlson@gmail.com>
2022-07-13 12:54:52 -0700

    changed spinner

--- ghi/src/index.js
+++ ghi/src/index.js
@@ -1,41 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Wheel from './components/wheel';
-import './styles.css';
-
-export class App extends React.Component {
-  constructor() {
-    super();
-    this.places = [
-      'Cardio',
-      'Chest',
-      'Back',
-      'Shoulders',
-      'Upper arms',
-      'Upper legs'
-  ];
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>What should you eat today?</h1>
-        <Wheel items={this.places} />
-      </div>
-    );
-  }
-}
-
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
 
 
 
-
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

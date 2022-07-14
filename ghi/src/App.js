26ec59b4c9a022cb0e9b899ba378f0a3cc8fae20
perlamcarlson <perlamcarlson@gmail.com>
2022-07-14 14:54:53 -0700

    fixing merge conflicts

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,57 +1,30 @@
-<<<<<<< HEAD
-
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import Nav from './Nav';
-import MainPage from './MainPage';
-// import Spinner from './Spinner';
-
-
-=======
->>>>>>> main
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import SignUpForm from './SignUpForm';
 import Nav from './Nav';
 import LoginForm from './LoginForm';
+import MainPage from './MainPage';
+// import Spinner from './Spinner';
 
 function App(props) {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
     <Nav />
     <div className="container">
       <Routes>
         <Route path="/" element={<MainPage />} />
-      
-        {/* <Route path="/">
-          <Route path="" element={<Spinner />} />
-        </Route> */}
-        
-        </Routes>
-    </div>
-    
-    </BrowserRouter>
-     
-
-  )
-=======
-        <Nav />
-        <div className="main_container">
-          <Routes>
-            <Route path="/signup">
-              <Route path="new" element={<SignUpForm signup={props.signup} />} />
-            </Route>
+        <Route path="/signup">
+          <Route path="new" element={<SignUpForm signup={props.signup} />} />
+        </Route>
             <Route path="/login">
               <Route path="" element={<LoginForm login={props.login} />} />
             </Route>
           </Routes>
-        
-        
-        </div>
+      </div>
     </BrowserRouter>
   );
->>>>>>> main
 }
-
 export default App;
+
+/* <Route path="/">
+          <Route path="" element={<Spinner />} />*/

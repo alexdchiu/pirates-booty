82493d918b7163a4f45143bfb2be228b79a320ce
shelen ells <shelen.ells@gmail.com>
2022-07-14 15:05:02 -0700

    update to views main

--- accounts/users/api_views.py
+++ accounts/users/api_views.py
@@ -45,59 +45,6 @@ def api_user(request):
 
 
 
-
-
-
-
-
-
-
-
-# @require_http_methods(["PUT"])
-# def create_user(json_content):
-#     try:
-#         content = json.loads(json_content)
-#     except json.JSONDecodeError:
-#         return 400, {"message": "Bad JSON"}, None
-
-#     required_properties = [
-#         "username",
-#         "email",
-#         "password",
-#         "first_name",
-#         "last_name",
-#     ]
-#     missing_properties = []
-#     for required_property in required_properties:
-#         if (
-#             required_property not in content
-#             or len(content[required_property]) == 0
-#         ):
-#             missing_properties.append(required_property)
-#     if missing_properties:
-#         response_content = {
-#             "message": "missing properties",
-#             "properties": missing_properties,
-#         }
-#         return 400, response_content, None
-
-#     try:
-#         account = User.objects.create_user(
-#             username=content["username"],
-#             email=content["email"],
-#             password=content["password"],
-#             first_name=content["first_name"],
-#             last_name=content["last_name"],
-#         )
-#         return 200, account, account
-#     except IntegrityError as e:
-#         return 409, {"message": str(e)}, None
-#     except ValueError as e:
-#         return 400, {"message": str(e)}, None
-
-
-
-
 # @require_http_methods(["PUT"])
 # def api_increment_coin(requests, pk):
 #     increase = User.objects.get(id=pk)

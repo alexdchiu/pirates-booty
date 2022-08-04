3560f033af94a4d20bf72bbd2a5ee6a9cb06ed42
Shelen Ells <shelen.ells@gmail.com>
2022-08-04 17:36:40 +0000

    Revert "working tests folder, must run in CLI, and ensure the pgadmin has permission..."
    This reverts commit 41e24e87b79a9a91932ae6ead93047de00ec2a2c

--- accounts/users/tests.py
+++ accounts/users/tests.py
@@ -1,15 +1,17 @@
-from django.test import TestCase, Client
+from django.test import TestCase
+
+# Create your tests here.
 
 class FeatureTests(TestCase):
     def test_black_installed(self):
         try:
-            import black  
+            import black  # noqa: F401
         except ModuleNotFoundError:
             self.fail("Could not find 'black' installed in the environment")
 
     def test_flake8_installed(self):
         try:
-            import flake8  
+            import flake8  # noqa: F401
         except ModuleNotFoundError:
             self.fail("Could not find 'flake8' installed in the environment")
 

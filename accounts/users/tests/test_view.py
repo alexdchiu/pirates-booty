804e23589df55bb4612c8f09cb97883720115784
perlamcarlson <perlamcarlson@gmail.com>
2022-08-05 10:55:47 -0700

    added tests completed workout models exists and completed workout model has integer field

--- accounts/users/tests/test_view.py
+++ accounts/users/tests/test_view.py
@@ -1,3 +1,4 @@
+from ast import Mod
 from django.test import TestCase, Client
 
 class FeatureTests(TestCase):
@@ -26,4 +27,32 @@ class FeatureTests(TestCase):
             self.assertIn("users.apps.UsersConfig", INSTALLED_APPS)
         except ModuleNotFoundError:
             self.fail("Could not find 'users' installed in 'accounts'")
+
+    def test_completed_workout_model_exists(self):
+        try:
+            from users.models import Completed_Workout
+        except ModuleNotFoundError:
+            self.fail("could not find 'users.models.Completed_Workout")
+
+    def test_completed_workout_model_has_integer_field(self):
+        try:
+            from users.models import models, Completed_Workout
+
+            workout_id = Completed_Workout.workout_id
+            self.assertIsInstance(
+                workout_id.field,
+                models.IntegerField,
+                msg="Completed_Workout.workout_id should be an integer field",
+            )
+        except ModuleNotFoundError:
+            self.fail("Could not find 'completed_workout.models'")
+        except ImportError:
+            self.fail("Could not find 'completed_workout.models.Completed_Workout'")
+        except AttributeError:
+            self.fail("Could not find 'Completed_Workout.workout_id'")
+        
+
+            
+    
+
 
